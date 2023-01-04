52f52396d3cba8a8ae46f52b0c8225383aea024a
Arnaud-Dlt <arnauddeletre@outlook.fr>
2023-01-04 10:12:34 +0000

    11h15

---
+++ ExTab/index.js
@@ -0,0 +1,74 @@
+//////////////////  EXO1  ////////////////////
+
+/*
+let animals = ["Dog", "Cat", "Spider", "Bat", "Snake", "Bear"];
+
+for(let i=0;i<animals.length;i++)
+{
+    console.log(animals[i]);
+}
+
+let gods = ["Zeus", "Persephone", "Dyonisos", "Artemis", "Ares"];
+
+for(god of gods)
+{
+    console.log(god);
+}
+*/
+
+
+//////////////////  EXO2  ////////////////////
+
+
+/*
+let animals_even = ["Dog", "Cat", "Spider"];
+
+for(let i=0;i<animals_even.length;i++)
+{
+    if(i%2===0)
+    {
+        console.log(animals_even[i]);
+    }
+}
+
+let animals_odd = ["Bat", "Snake", "Bear"];
+
+for(let i=0;i<animals_odd.length;i++)
+{
+    if(i%2===1)
+    {
+        console.log(animals_odd[i]);
+    }
+}
+*/
+
+
+
+//////////////////  EXO3  ////////////////////
+/*
+
+let numbers = [10, 11, 15, 6];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+}
+
+console.log(sum);
+*/
+
+
+//////////////////  EXO4  ////////////////////
+
+let grades = [[20, 10], [15, 20], [13, 18]];
+
+let average = 0;
+
+for(let i=0; i<grades.length;i++)
+{
+    correction svp
+}
+
+console.log(average);
+
