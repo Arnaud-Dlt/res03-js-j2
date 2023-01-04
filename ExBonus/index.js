a252cc975eb6efd9727ee1569db0f7f9f8f54922
Arnaud-Dlt <arnauddeletre@outlook.fr>
2023-01-04 16:18:12 +0000

    17h30

---
+++ ExBonus/index.js
@@ -0,0 +1,14 @@
+/*
+Créer une fonction sortNumbers qui retourne un tableau avec les nombres du tableau numbers triés du plus petit au plus grand.
+Vous n'avez pas le droit d'utiliser Array.sort ou toute autre fonction de tri fournie par JavaScript.
+*/
+
+function sortNumbers() {
+    
+    let numbers = [-23,42,21,90,54,362,-12,57,78,84,35,546,128,7,15,65,28];
+    
+    for (let i = 0; i < numbers.length; i++) 
+    {
+        
+    }
+}
